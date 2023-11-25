7574985de75e2c815730fa1a242ee6390529804b
Ike Gomber <isaacbermude14@gmail.com>
2023-11-24 22:52:52 -0500

    Añadí el código js para guardar los libros en un objeto de clase libro y est objeto guardarlo en un array

--- script.js
+++ script.js
@@ -1,6 +1,65 @@
-const book1 = {
-  "name": "Las Cronicas de narnia",
-  "author": "CS Lewis"
-}
-const myLibrary = [];
+let library = [];
 
+class Book {
+  constructor(name, author, pages, status) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
+}
+function adminPopupNewBook() {
+  let btnOpenPopup = document.getElementById("newBook");
+  let btnSubmit = document.getElementById("btn-submit");
+  let cancelButton = document.getElementById("cancel");
+  let nBookPopup = document.getElementById("new-book-popup");
+  nBookPopup.style.display = "none";
+  btnOpenPopup.addEventListener("click", function () {
+    nBookPopup.showModal();
+    nBookPopup.style.display = "flex";
+  });
+  btnSubmit.addEventListener("click", function () {
+    nBookPopup.style.display = "none";
+    let name = document.querySelector("#book-name").value;
+    let author = document.querySelector("#author-name").value;
+    let pages = document.querySelector("#number-pages").value;
+    let status;
+    if (document.querySelector("#estatus").checked) {
+      status = "leido";
+    } else {
+      status = "no leido";
+    }
+    addBookToLibrary(name, author, pages, status);
+    document.getElementById("form-new-book").reset();
+  });
+  cancelButton.addEventListener("click", function () {
+    nBookPopup.close();
+    nBookPopup.style.display = "none";
+  });
+  document.addEventListener("keydown", (event) => {
+    if (event.key == "Escape") {
+      nBookPopup.style.display = "none";
+    }
+  });
+  displayBooks(library);
+
+}
+adminPopupNewBook();
+
+function displayBooks(library) {
+  
+}
+
+function deleteBook() {
+  let btnDeleteBook = document.querySelector(".delete-btn");
+  btnDeleteBook.addEventListener("click", function () {
+    let trBook = btnDeleteBook.parentNode.parentNode.remove();
+  });
+}
+deleteBook();
+
+
+function addBookToLibrary(name, author, pages, status) {
+  let book = new Book(name, author, pages, status);
+  library.push(book);
+}
